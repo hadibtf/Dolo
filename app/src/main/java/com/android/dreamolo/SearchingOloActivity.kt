eaad4b06254ce68b95c2dc94f0af05377ad4fa13
hadibastanfar <98bastanfar@gmail.com>
2019-02-20 02:25:14 +0330

    Searching Olo layout done

--- app/src/main/java/com/android/dreamolo/SearchingOloActivity.kt
+++ app/src/main/java/com/android/dreamolo/SearchingOloActivity.kt
@@ -1,12 +1,75 @@
 package com.android.dreamolo
 
+import android.animation.ObjectAnimator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_searching_olo.*
+import tools.Assistant
+import android.view.animation.RotateAnimation
+
 
 class SearchingOloActivity : AppCompatActivity() {
 
+    lateinit var assistant: Assistant
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        assistant = Assistant(this)
         setContentView(R.layout.activity_searching_olo)
+        yesBt.setOnClickListener {
+            expandedLy.expand(true)
+            expandedLy2.expand(true)
+            val fadeOut = AlphaAnimation(1f, 0f)
+            fadeOut.interpolator = AccelerateInterpolator() //and this
+            fadeOut.duration = 300
+//            fadeOut.isFillEnabled = true
+//            fadeOut.fillAfter = true
+            yesNoLy.startAnimation(fadeOut)
+            fadeOut.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    yesNoLy.visibility = View.GONE
+                    yesBt.isClickable = false
+                    noBt.isClickable = false
+                    oloCodeLy.visibility = View.VISIBLE
+                }
+
+                override fun onAnimationStart(animation: Animation?) {
+                }
+
+            })
+        }
+        cancelOloCodeBt.setOnClickListener {
+            expandedLy2.collapse(true)
+            expandedLy.collapse(true)
+            yesNoLy.visibility = View.VISIBLE
+            yesBt.isClickable = true
+            noBt.isClickable = true
+            oloCodeLy.visibility = View.GONE
+
+        }
+
+        val rotate = RotateAnimation(
+            0f, 360f,
+            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+
+        rotate.duration = 60000
+        rotate.repeatMode = Animation.REVERSE
+        rotate.repeatCount = Animation.INFINITE
+        earthView.animation = rotate
     }
 }
